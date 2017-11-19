c4fea8a50d5a5572c09fd5d0d38a086549869759
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-11-19 18:11:00 +0100

    Generación de la estructura carpetas y activity_main en todos sus formatos

--- app/src/main/java/com/mcolmenero/leakycauldron/activity/MainActivity.kt
+++ app/src/main/java/com/mcolmenero/leakycauldron/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mcolmenero.leakycauldron
+package com.mcolmenero.leakycauldron.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mcolmenero.leakycauldron.R
 
 class MainActivity : AppCompatActivity() {
 
